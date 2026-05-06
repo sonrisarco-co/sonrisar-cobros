724232f2e16a2ddec472bb4944a1748b4f131ae2
sonrisarco-co <sonrisarco@gmail.com>
2026-05-06 06:40:24 -0300

    Corrige DB Postgres Render

--- sonrisar_cobros/settings.py
+++ sonrisar_cobros/settings.py
@@ -87,11 +87,14 @@ WSGI_APPLICATION = 'sonrisar_cobros.wsgi.application'
 
 
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=False
+    )
 }
 
 

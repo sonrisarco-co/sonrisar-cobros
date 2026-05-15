b98028f15e8d97dab44a4014e96caca85217c3d8
sonrisarco-co <sonrisarco@gmail.com>
2026-05-14 22:39:54 -0300

    Agregar admin de gastos

--- pagos/admin.py
+++ pagos/admin.py
@@ -1,8 +1,43 @@
 from django.contrib import admin
-from .models import Pago
+
+from .models import Pago, Gasto
+
 
 @admin.register(Pago)
 class PagoAdmin(admin.ModelAdmin):
-    list_display = ("fecha", "paciente", "monto", "metodo")
-    list_filter = ("metodo",)
-    search_fields = ("paciente", "concepto")
+    list_display = (
+        "fecha",
+        "paciente",
+        "monto",
+        "metodo",
+        "concepto",
+    )
+
+    search_fields = (
+        "paciente",
+        "concepto",
+    )
+
+    list_filter = (
+        "metodo",
+        "fecha",
+    )
+
+
+@admin.register(Gasto)
+class GastoAdmin(admin.ModelAdmin):
+    list_display = (
+        "fecha",
+        "concepto",
+        "monto",
+        "metodo",
+    )
+
+    search_fields = (
+        "concepto",
+    )
+
+    list_filter = (
+        "metodo",
+        "fecha",
+    )
